5d3e0ccc916bf0266c33f01324f3b861f3087097
Eduardo Peschke <eduardopeschke@gmail.com>
2021-12-12 14:23:34 -0300

    [leap-year] add views + template

--- all_the_small_things/small_things/views.py
+++ all_the_small_things/small_things/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+def leap_year_view(request):
+	# GET
+
+	# Get today
+	today = date.today().strftime("%d/%m/%Y")
+	actual_year = int(today.split('/')[-1])
+
+	# Get if year is leap
+	leap = leap_year(actual_year)
+
+	# Data to client side
+	context = {
+		'actual_year': actual_year,
+		'is_leap': leap
+	}
+	return render(request, 'small_things/leap-year.html', context)
+
 def leap_year(year):
 	is_leap = False
 	if year % 4 == 0:
