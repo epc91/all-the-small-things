45f986788d005754dd3f411f37cf10fa476c84a5
Eduardo Peschke <eduardopeschke@gmail.com>
2021-12-12 14:22:56 -0300

    [leap-year] add the main function to calculate leap years.

--- all_the_small_things/small_things/views.py
+++ all_the_small_things/small_things/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def leap_year(year):
+	is_leap = False
+	if year % 4 == 0:
+		is_leap = True
+		if year % 100 == 0 and year % 400 == 0:
+			is_leap = True
+		else:
+			is_leap = False
+	else:
+		is_leap = False
+	return is_leap
